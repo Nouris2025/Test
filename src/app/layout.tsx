b355764ba453d67efd0bad55649d7d4b3009f8e2
Nouris2025 <nouriselbahrferriess.contact@gmail.com>
2025-02-05 18:14:50 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import DefaultLayout from "./layouts/default";
 
 const inter = Inter({ subsets: ["latin"] });
 
